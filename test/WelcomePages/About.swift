bb705787b9a1b215ecd7c86b1e0af581d523a800
domheaton <dhh1g15@soton.ac.uk>
2018-02-05 20:01:01 +0000

    v4.1 -- AES Encryption demo added to About page for testing

--- test/WelcomePages/About.swift
+++ test/WelcomePages/About.swift
@@ -14,10 +14,57 @@ import CryptoSwift
 
 class About: UIViewController, UITextFieldDelegate {
     
+    let key: Array<UInt8> = [0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00]
+    
+    let iv: Array<UInt8> = AES.randomIV(AES.blockSize)
+
+    var encrypted: [UInt8]?
+    
     @IBOutlet weak var label: UILabel!
     @IBOutlet weak var textField: UITextField!
+    
+    //Decrypt Function
+    @IBAction func decryptButton(_ sender: UIButton) {
+        do {
+            //Decrypt using AES
+            let decrypted = try AES(key: key, blockMode: .CBC(iv: iv), padding: .pkcs7).decrypt(encrypted!)
+            
+            //From UInt8 to String
+            let decryptedData = String(bytes: Data(decrypted), encoding: .utf8)
+            
+            //Display encrypted value on label
+            label.text = decryptedData!
+            
+            //For Debugging
+            print("C/T*: ", encrypted!)
+            print("P/T*: ", decryptedData!)
+        
+        } catch {
+            print(error)
+        }
+    }
+    
+    //Encrypt Function
     @IBAction func button(_ sender: UIButton) {
-        label.text = textField.text!.sha512()
+        //          SHA512 Function
+        //          label.text = textField.text!.sha512()
+        
+        let input: [UInt8] = Array(textField.text!.utf8)
+        
+        do {
+            //encrypt using AES
+            encrypted = try AES(key: key, blockMode: .CBC(iv: iv), padding: .pkcs7).encrypt(input)
+            
+            //Display encrypted value on label
+            label.text = "\(encrypted!)"
+            
+            //For Debugging
+            print("P/T : ", textField.text!)
+            print("C/T : ", encrypted!)
+
+        } catch {
+            print(error)
+        }
     }
 
     override func viewDidLoad() {
@@ -39,4 +86,3 @@ class About: UIViewController, UITextFieldDelegate {
     }
     
 }
-
